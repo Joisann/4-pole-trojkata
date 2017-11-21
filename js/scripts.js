8488f890e9b5bee8c1e994595d14d9b84d62b8dc
Joisann <joisann777@gmail.com>
2017-11-21 19:45:55 +0100

    changing

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var a = 5;
-    h = 4;
+var a = 5,
+    h = 4,
     triangleArea = a*h/2;
 
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea)
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
